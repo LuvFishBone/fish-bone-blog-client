bd20f5026773086e7e8ca6c17177d610d71a8181
luvfishbone <luvfishbone@gmail.com>
2018-09-21 23:59:03 +0800

    1.0.0 - right asider 结构调整，增加tags组件

--- client/src/components/TagList.vue
+++ client/src/components/TagList.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="taglist">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M368.5 240H272v-96.5c0-8.8-7.2-16-16-16s-16 7.2-16 16V240h-96.5c-8.8 0-16 7.2-16 16 0 4.4 1.8 8.4 4.7 11.3 2.9 2.9 6.9 4.7 11.3 4.7H240v96.5c0 4.4 1.8 8.4 4.7 11.3 2.9 2.9 6.9 4.7 11.3 4.7 8.8 0 16-7.2 16-16V272h96.5c8.8 0 16-7.2 16-16s-7.2-16-16-16z"/></svg>
         <a href="#" v-for="item in list" :class="[{tag: true}, tagAttr[item.color]]"  :key="item.id">{{item.name}}</a>
     </div>
 </template>
